b9ed576548b846ee7a96725ed001853e96e3c7f8
rased100 <rased100@gmail.com>
2021-11-12 08:42:56 +0600

    product_id

--- index.js
+++ index.js
@@ -3,9 +3,11 @@ const app = express()
 const cors = require('cors');
 require('dotenv').config();
 const { MongoClient } = require('mongodb');
+const ObjectId = require('mongodb').ObjectId;
 const port = process.env.PORT || 5000;
 
 app.use(cors());
+app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.i4mso.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 
@@ -22,6 +24,14 @@ async function run() {
             const cursor = productCollection.find({});
             const products = await cursor.toArray();
             res.send(products);
+        });
+
+        app.get('/product/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const product = await productCollection.findOne(query);
+            console.log('load product with id: ', id);
+            res.send(product);
         })
     }
     finally {
